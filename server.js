93befd3a9c539a71de53eb11270a60a6f34ebe8c
n14-py <luisfretes805@gmail.com>
2025-12-25 18:46:49 -0300

    white

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const PORT = process.env.PORT || 3000;
 // =============================================
 const whiteList = [
     'https://lfaftechapi.onrender.com',
+    'https://lfaftechapi-7nrb.onrender.com',
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'http://127.0.0.1:5501',
