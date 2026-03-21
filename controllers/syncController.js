6f0d2cdb7612357df73bcbaaccf9d1d325fcf174
n14-py <luisfretes805@gmail.com>
2026-03-21 13:19:40 -0300

    new s

--- controllers/syncController.js
+++ controllers/syncController.js
@@ -10,7 +10,7 @@ const { generateArticleContent } = require('../utils/geminiClient');
 // ⚙️ 1. CONFIGURACIÓN DE LA FLOTA DE BOTS (VIDEO WORKERS) 1 es short
 // ============================================================================
 const VIDEO_BOT_URLS = [
-    "http://3.20.238.151:3001",
+    "http://18.188.34.160:3001",
     "http://3.15.176.240:3001"
 ];
 
